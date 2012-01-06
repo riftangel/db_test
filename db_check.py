aaa65047b9ccf6eb14a9259956c509c2582f0db2
riftangel <rift.angel@gmail.com>
2012-01-06 17:08:04 -0600

    New pwd dialog and meta pwd prompt

--- db_check.py
+++ db_check.py
@@ -16,6 +16,7 @@ from Crypto.Cipher import AES
 try:
     import curses
     import curses.panel
+    import curses.ascii
 except:
     print '* curses import error : not available on your system'
 #
@@ -158,7 +159,24 @@ def initialize_curses_vars(first=False):
 
     # Display fixed info (e.g. Global labels)
     prepare_display()
-    
+
+#
+# Show/Hide All Panel
+#
+def show_all_panels(really=True):
+
+    if really:
+        scrpan.show()
+        labelpan.show()
+        fieldpan.show()
+    else:
+        scrpan.hide()
+        labelpan.hide()
+        fieldpan.hide()
+        
+    curses.panel.update_panels()
+    curses.doupdate()
+        
 # Make sure to reset terminal to original state
 # When curses wrapper is used this is not required.
 def reset_terminal():
@@ -214,14 +232,21 @@ class table_window():
     def __init__(self):
         # Compute location in parent window
         self.scrh, self.scrw = scrwin.getmaxyx()
-        self.tab_h = 20
-        self.tab_w = 24
+        self.tab_h = 40
+        self.tab_w = 48
         self.tab_x = (self.scrw - self.tab_w) / 2
         self.tab_y = (self.scrh - self.tab_h) / 2
 
+        self.tab_pos_x = 0
+        self.tab_pos_y = 0
+        
+        # TBD: May be able bypass win/pad
         self.tab_win = curses.newwin(self.tab_h, self.tab_w, self.tab_y, self.tab_x)
         self.tab_pan = curses.panel.new_panel(self.tab_win)
 
+        # TBD: Need to handle border (add to tab_h, tab_w)
+        self.tab_pad_border = 1
+        
     def populate(self):
 
         col0_content = ('row of data\n' * 30).split('\n')
@@ -234,9 +259,9 @@ class table_window():
             else:
                 col2_content.append( False )
         # Name, Width, Type, Attributes (bit_flags), content]
-        tab = [ [ 'column_name0', 8, type(""), 0,  col0_content ], \
-                [ 'column_name1', 4, type(0), 0, col1_content ], \
-                [ 'column_name2', 2, type(True), 0, col2_content] ]
+        tab = [ [ 'column_name0', 16, type(""), 0,  col0_content ], \
+                [ 'column_name1', 12, type(0), 0, col1_content ], \
+                [ 'column_name2', 25, type(True), 0, col2_content] ]
         self.tab_def = tab
         
         # Compute size of all columns plus L/R borders
@@ -257,48 +282,84 @@ class table_window():
             self.tab_all_col_w = self.tab_w
         if self.tab_all_row_h < self.tab_h:
             self.tab_all_row_h = self.tab_h
-            
-        self.tab_pad = curses.newpad( self.tab_all_col_w , self.tab_all_row_h )
-        #        for y in range(0, self.tab_all_col_w):
-        #    for x in range(0, self.tab_all_row_h):
-        #        try: self.tab_pad.addch( y,x, ord('a') + (x*x + y*y) % 26)
-        #        except curses.error: pass
+
+        # newpad(nlines, ncolumns)
+        self.tab_pad = curses.newpad( self.tab_all_row_h , self.tab_all_col_w )
         
     def refresh(self):
 
         # Start at first/column 0
         self.tab_col_index = 0
 
+        # Color pair is 1 base
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_GREEN)
+        curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED)
+        
         # Compute pannel for each column
-        # ...
+        pad_col_pos = 0         # TBD: Support border (per column? active ?)
+        pad_col_idx = 0
+        col_color_index = 1
+        for col_def in self.tab_def:
+            pad_row_pos = 0
+            col_width = col_def[1]
+            col_pad  = col_width * ' '            
+            col_text = col_def[0] + col_pad
+            self.tab_pad.addnstr(pad_row_pos, pad_col_pos, col_text, col_width, \
+                                 curses.color_pair(col_color_index) )
+            for row_data in col_def[4]:                
+                pad_row_pos += 1
+                row_selected = 0
+                if pad_row_pos == self.tab_pos_y and pad_col_idx == self.tab_pos_x:
+                    row_selected = curses.A_BOLD
+                self.tab_pad.addnstr(pad_row_pos, pad_col_pos, str(row_data)+col_pad, col_width, \
+                                     curses.color_pair(col_color_index) + row_selected)                
+            pad_col_pos += col_width
+            col_color_index +=1
+            pad_col_idx += 1
 
-    def display(self):
-        
-        fieldwin.addnstr(1, 0, str(self.tab_all_col_w), fieldw, curses.A_BOLD)
-        fieldwin.addnstr(2, 0, str(self.tab_all_row_h), fieldw, curses.A_BOLD)
-        curses.panel.update_panels()
-
-        #        self.tab_pad.box()
-        self.tab_pad.addnstr(1,0,"0123456789"*4, self.tab_w-4, curses.A_BOLD)
-        self.tab_pad.addnstr(2,1,str(self.tab_all_col_w), self.tab_w-4, curses.A_BOLD)
-        self.tab_pad.addnstr(3,1,str(self.tab_all_row_h), self.tab_w-4, curses.A_BOLD)
-        self.tab_pad.addnstr(4,1,str(self.tab_w), self.tab_w-4, curses.A_BOLD)
-        self.tab_pad.addnstr(5,1,str(self.tab_h), self.tab_w-4, curses.A_BOLD)
-        
-        #self.tab_win.box()
-        self.tab_pad.box()
+        # pad.refresh( orig_row, orig_col, min_row, min_col, max_row, max_col )
         self.tab_pad.refresh( 0, 0, self.tab_y, self.tab_x, \
-                              self.tab_y+self.tab_w+2, self.tab_x+self.tab_h )
-        curses.panel.update_panels()
-        curses.doupdate()
+                              self.tab_y+self.tab_h, self.tab_x+self.tab_w )
         
+    def display(self):
+        self.refresh()
+
+    # Handle key functions
+    def process_up(self):
+        if self.tab_pos_y:
+            self.tab_pos_y -= 1
+        
+    def process_down(self):
+        self.tab_pos_y += 1
+
+    def process_right(self):
+        self.tab_pos_x += 1
+
+    def process_left(self):
+        if self.tab_pos_x:
+            self.tab_pos_x -= 1
+            
     def navigate(self):
         
-        self.tab_win.keypad(1)
+        self.tab_pad.keypad(1)
+        self.key = -1
+        self.valid_keys = { curses.KEY_UP   : self.process_up, \
+                            curses.KEY_DOWN : self.process_down, \
+                            curses.KEY_RIGHT: self.process_right, \
+                            curses.KEY_LEFT : self.process_left }        
         while True:
-            self.key = self.tab_win.getch()
+            self.key = self.tab_pad.getch()
+            if self.key != -1:
+                # Debug
+                fieldwin.addstr(3, 0, 'key : '+str(self.key)+'   ', curses.A_BOLD)
+                curses.panel.update_panels()
+                curses.doupdate()
             if self.key == ord('\n'):
                 break
+            if self.key in self.valid_keys.keys():
+                self.valid_keys[ self.key ]()
+                self.refresh()
             
 class test_dialog():
 
@@ -442,13 +503,104 @@ class test_dialog():
         
         curses.panel.update_panels()
         curses.doupdate()
-            
+
+#
+# Forms
+#
+
+#
+# Basic Password Window
+# Only ASCII characters (32-126) allowed for now
+# Basic ^H/BS/DEL(ETE) key support
+#
+class password_window():
+
+    def __init__(self, x, y, width):
+        self.win_x = x
+        self.win_y = y
+        self.win_w = width
+        self.passwd = bytearray(' ' * (self.win_w - 2))
+        self.show_passwd = False
+        self.inkey = -1
+        
+    def display(self):
+        self.win = curses.newwin( 3, self.win_w, self.win_y,self.win_x)
+        self.win.box()
+        self.win.move(1,1)
+        self.win.keypad(1)
+        curses.doupdate()
+
+    def refresh(self):
+        self.win.box()
+        if self.inkey != -1:
+            self.win.addstr(0,0,str(self.inkey))  
+        if self.show_passwd:
+            self.pwd_str = str(self.passwd)[:self.cur_pos] + ' ' * self.win_w
+        else:
+            self.pwd_str = self.cur_pos * '*' + ' ' * self.win_w
+        self.win.addnstr(1,1,self.pwd_str, self.win_w-2)
+        self.win.move(1,self.cur_pos+1)
+        
+    def getpwd(self,show_passwd=False):
+        self.show_passwd = show_passwd
+        inkey = 0
+        self.cur_pos = 0
+        while True:
+            self.inkey = inkey = self.win.getch()
+            if inkey in (curses.ascii.BS, curses.KEY_BACKSPACE,curses.KEY_DC,curses.ascii.DEL):
+                self.passwd[self.cur_pos] = ' '
+                if self.cur_pos: self.cur_pos -= 1
+                self.refresh()
+                continue
+            if inkey == ord('\n'):
+                break
+            if self.cur_pos == self.win_w - 2 - 1:
+                self.refresh()
+                continue
+            if not (inkey >= 32 and inkey <= 126):
+                self.refresh()
+                continue
+            self.passwd[self.cur_pos] = chr(inkey&0xFF)
+            self.cur_pos += 1
+            self.refresh()
+        return str(self.passwd)[:self.cur_pos]
+
+#
+# Ask for Meta PWD before starting the App
+#
+def ask_meta_pwd(meta_hello='Please enter Meta Password'):
+
+    scrh, scrw = scrwin.getmaxyx()
+    pww = 32
+    pwh = 3
+    pwx = (scrw - pww) / 2
+    pwy = (scrh - pwh) / 2
+
+    show_all_panels(really=False)
+    scrwin.clear()
+    win = curses.newwin(1, pww, pwy-2,pwx)
+    panel = curses.panel.new_panel(win)
+    panel.top()
+    panel.show()
+    pad = (((pww-2) - len(meta_hello))/2)*' '
+    win.addnstr(0,0,meta_hello+pad,pww-2)
+    curses.panel.update_panels()
+    curses.doupdate()
+    
+    pww = password_window(pwx,pwy,pww)
+    pww.display()
+    pwd = pww.getpwd()
+    
 if __name__ == '__main__':
     print 'DB Check'
 
     initialize_curses_vars(True)
-
-#    dialog = test_dialog()
+#
+# Meta Pwd
+#
+    ask_meta_pwd()
+    
+# dialog = test_dialog()
 #    dialog.display()
 
     tab = table_window()
